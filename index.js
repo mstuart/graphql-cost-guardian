ccb647616b415dd75ad78d60f4cf3b0e79313575
Mark Stuart <stuartmark@gmail.com>
2026-08-09 22:40:52 -0700

    chore: migrate to Ultracite (#15)
    * chore: migrate to ultracite
    
    * chore: enforce ultracite
    
    ---------
    
    Co-authored-by: Mark Stuart <mstuart@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,105 +1,112 @@
-import {parse, visit, Kind} from 'graphql';
+import { Kind, parse, visit } from "graphql";
 
 export class CostExceededError extends Error {
-	constructor(cost, maxCost) {
-		super(`Query cost ${cost} exceeds maximum allowed cost ${maxCost}`);
-		this.name = 'CostExceededError';
-		this.cost = cost;
-		this.maxCost = maxCost;
-	}
+  constructor(cost, maxCost) {
+    super(`Query cost ${cost} exceeds maximum allowed cost ${maxCost}`);
+    this.name = "CostExceededError";
+    this.cost = cost;
+    this.maxCost = maxCost;
+  }
 }
 
 export class DepthExceededError extends Error {
-	constructor(depth, maxDepth) {
-		super(`Query depth ${depth} exceeds maximum allowed depth ${maxDepth}`);
-		this.name = 'DepthExceededError';
-		this.depth = depth;
-		this.maxDepth = maxDepth;
-	}
+  constructor(depth, maxDepth) {
+    super(`Query depth ${depth} exceeds maximum allowed depth ${maxDepth}`);
+    this.name = "DepthExceededError";
+    this.depth = depth;
+    this.maxDepth = maxDepth;
+  }
 }
 
 export default function analyzeCost(query, options = {}) {
-	const {
-		fieldCosts = {},
-		defaultCost = 1,
-		depthCostFactor = 1,
-		listCostFactor = 10,
-		maxCost,
-		maxDepth,
-	} = options;
+  const {
+    fieldCosts = {},
+    defaultCost = 1,
+    depthCostFactor = 1,
+    listCostFactor = 10,
+    maxCost,
+    maxDepth,
+  } = options;
 
-	const document = typeof query === 'string' ? parse(query) : query;
+  const document = typeof query === "string" ? parse(query) : query;
 
-	let totalCost = 0;
-	let currentDepth = 0;
-	let maxObservedDepth = 0;
-	const fields = new Map();
-	const typeStack = [];
+  let totalCost = 0;
+  let currentDepth = 0;
+  let maxObservedDepth = 0;
+  const fields = new Map();
+  const typeStack = [];
 
-	visit(document, {
-		[Kind.OPERATION_DEFINITION]: {
-			enter(node) {
-				const operationType = node.operation ?? 'Query';
-				const typeName = operationType.charAt(0).toUpperCase() + operationType.slice(1);
-				typeStack.push(typeName);
-			},
-			leave() {
-				typeStack.pop();
-			},
-		},
-		[Kind.FIELD]: {
-			enter(node) {
-				const fieldName = node.name.value;
-				const parentType = typeStack.at(-1) ?? 'Unknown';
-				const qualifiedName = `${parentType}.${fieldName}`;
+  visit(document, {
+    [Kind.OPERATION_DEFINITION]: {
+      enter(node) {
+        const operationType = node.operation ?? "Query";
+        const typeName =
+          operationType.charAt(0).toUpperCase() + operationType.slice(1);
+        typeStack.push(typeName);
+      },
+      leave() {
+        typeStack.pop();
+      },
+    },
+    [Kind.FIELD]: {
+      enter(node) {
+        const fieldName = node.name.value;
+        const parentType = typeStack.at(-1) ?? "Unknown";
+        const qualifiedName = `${parentType}.${fieldName}`;
 
-				const hasListType = node.arguments?.some(argument => argument.name.value === 'first' || argument.name.value === 'last' || argument.name.value === 'limit');
+        const hasListType = node.arguments?.some(
+          (argument) =>
+            argument.name.value === "first" ||
+            argument.name.value === "last" ||
+            argument.name.value === "limit"
+        );
 
-				const depthMultiplier = depthCostFactor === 1 ? 1 : depthCostFactor ** currentDepth;
-				const baseCost = fieldCosts[qualifiedName] ?? defaultCost;
-				const listMultiplier = hasListType ? listCostFactor : 1;
-				const fieldCost = baseCost * depthMultiplier * listMultiplier;
+        const depthMultiplier =
+          depthCostFactor === 1 ? 1 : depthCostFactor ** currentDepth;
+        const baseCost = fieldCosts[qualifiedName] ?? defaultCost;
+        const listMultiplier = hasListType ? listCostFactor : 1;
+        const fieldCost = baseCost * depthMultiplier * listMultiplier;
 
-				totalCost += fieldCost;
+        totalCost += fieldCost;
 
-				const currentCount = fields.get(qualifiedName) ?? 0;
-				fields.set(qualifiedName, currentCount + fieldCost);
+        const currentCount = fields.get(qualifiedName) ?? 0;
+        fields.set(qualifiedName, currentCount + fieldCost);
 
-				typeStack.push(fieldName.charAt(0).toUpperCase() + fieldName.slice(1));
-			},
-			leave() {
-				typeStack.pop();
-			},
-		},
-		[Kind.SELECTION_SET]: {
-			enter() {
-				currentDepth++;
+        typeStack.push(fieldName.charAt(0).toUpperCase() + fieldName.slice(1));
+      },
+      leave() {
+        typeStack.pop();
+      },
+    },
+    [Kind.SELECTION_SET]: {
+      enter() {
+        currentDepth += 1;
 
-				if (currentDepth > maxObservedDepth) {
-					maxObservedDepth = currentDepth;
-				}
+        if (currentDepth > maxObservedDepth) {
+          maxObservedDepth = currentDepth;
+        }
 
-				if (maxDepth !== undefined && currentDepth > maxDepth) {
-					throw new DepthExceededError(currentDepth, maxDepth);
-				}
-			},
-			leave() {
-				currentDepth--;
-			},
-		},
-	});
+        if (maxDepth !== undefined && currentDepth > maxDepth) {
+          throw new DepthExceededError(currentDepth, maxDepth);
+        }
+      },
+      leave() {
+        currentDepth -= 1;
+      },
+    },
+  });
 
-	if (maxCost !== undefined && totalCost > maxCost) {
-		throw new CostExceededError(totalCost, maxCost);
-	}
+  if (maxCost !== undefined && totalCost > maxCost) {
+    throw new CostExceededError(totalCost, maxCost);
+  }
 
-	return {
-		cost: totalCost,
-		depth: maxObservedDepth,
-		fields,
-	};
+  return {
+    cost: totalCost,
+    depth: maxObservedDepth,
+    fields,
+  };
 }
 
 export function createCostMiddleware(options) {
-	return query => analyzeCost(query, options);
+  return (query) => analyzeCost(query, options);
 }
